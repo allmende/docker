d791f03502482d8fb5de2c5e2203da32e7464318
almereyda <almereyda@allmende.io>
2017-09-15 20:05:34 +0200

    Implement RabbitMQ example
    This commit implements an example for using Kanboard with RabbitMQ. It makes use of data volumes for the plugins directory, why it is detrimental to install the plugin by running
    
        docker-compose -f compose.alpine.postgres.rabbitmq.yaml run --rm --entrypoint /bin/bash app
        ./cli plugin:install https://github.com/kanboard/plugin-rabbitmq/releases/download/v1.0.0/RabbitMQ-1.0.0.zip
    
    or similarily getting the Plugin into the `plugins/` directory.

---
+++ files/kanboard/postgres_rabbitmq.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ENABLE_URL_REWRITE', true);
+
+define('DB_DRIVER', 'postgres');
+define('DB_USERNAME', 'kanboard');
+define('DB_PASSWORD', 'kanboard');
+define('DB_HOSTNAME', 'database');
+define('DB_NAME', 'kanboard');
+
+define('RABBITMQ_HOSTNAME', 'rabbitmq');
